4ebdc1d3d3fe17f057b40ab6948b2ac5adfbc7de
Luis Dier Luque <le.dier.luque@gmail.com>
2024-12-17 12:14:36 -0500

    actualizacion multipantalla 17122024 1214

--- resources/views/centrosreferencia/preanalitica/menu/index.blade.php
+++ resources/views/centrosreferencia/preanalitica/menu/index.blade.php
@@ -12,31 +12,7 @@
                                 <span class="navi-icon mr-2">
                                     <i class="fa fa-tags fa-3x"></i>
                                 </span>
-                                <span class="navi-text font-size-xl">Resultados registrados</span>
-                            </a>
-                        </div>
-                        <div class="col-md-3 navi-item" align="center">
-                            <a href="{{ route('visorresultado.index') }}" class="navi-link py-4 {{ active('visorresultado.index') }}">
-                                <span class="navi-icon mr-2">
-                                    <i class="fa fa-signal fa-3x"></i>
-                                </span>
-                                <span class="navi-text font-size-xl">Estadisticas</span>
-                            </a>
-                        </div>
-                        <div class="col-md-3 navi-item" align="center">
-                            <a href="{{ route('visorresultado.index') }}" class="navi-link py-4 {{ active('visorresultado.index') }}">
-                                <span class="navi-icon mr-2">
-                                    <i class="fa fa-spinner fa-3x"></i>
-                                </span>
-                                <span class="navi-text font-size-xl">Georeferencia</span>
-                            </a>
-                        </div>
-                        <div class="col-md-3 navi-item" align="center">
-                            <a href="{{ route('responsable.index') }}" class="navi-link py-4 {{ active('responsable.index') }}">
-                                <span class="navi-icon mr-2">
-                                    <i class="fa fa-users fa-3x"></i>
-                                </span>
-                                <span class="navi-text font-size-xl">Responsables</span>
+                                <span class="navi-text font-size-xl">Muestras registrados</span>
                             </a>
                         </div>
                     </div>
