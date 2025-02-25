52aaa8c4742b191a8773fa2815bd050d18d5e4ec
Luis Dier Luque <le.dier.luque@gmail.com>
2025-02-25 10:06:05 -0500

    Actualiza Index Postanalitica  25022025 1005 LD

--- app/Http/Livewire/Centrosreferencia/Postanalitica/Index.php
+++ app/Http/Livewire/Centrosreferencia/Postanalitica/Index.php
@@ -118,7 +118,7 @@ class Index extends Component
                         $this->fechafin='';
                     }
                     if($this->controlf==1){
-                        $analiticas = $analiticas->where('fecha_toma', '>=', $this->fechainicio)->where('fecha_toma','<=',$this->fechafin);
+                        $analiticas = $analiticas->where('fecha_toma_primera', '>=', $this->fechainicio)->where('fecha_toma_primera','<=',$this->fechafin);
                         $count = $analiticas->count();
 
                     }
