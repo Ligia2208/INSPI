32dc0d830e9dfa998baed5a495ef543868289b82
userx1 <userx1@gua.local>
2025-04-10 08:46:40 -0500

    10/4/2025 Robinson

---
+++ app/Http/Controllers/CentrosReferencia/Preanaliticagen/PreanaliticagenController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\CentrosReferencia\Preanaliticagen;
+
+use App\Http\Controllers\Controller;
+use App\Models\CentrosReferencia\Preanalitica;
+use App\Models\CentrosReferencia\Preanaliticagen;
+use App\Models\CentrosReferencia\Analitica;
+use Illuminate\Http\Request;
+
+class PreanaliticagenController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['permission:preanaliticagen']);
+    }
+
+    public function index(){
+        return view('centrosreferencia.preanaliticagen.index');
+    }
+
+    public function create(){
+        $preanalitica = new Preanaliticagen();
+        return view('centrosreferencia.preanaliticagen.create', compact('preanalitica'));
+    }
+
+    public function show(Preanaliticagen $Preanaliticasmico){
+        $analitica = Analitica::where('estado','=','A')->where('preanalitica_id','=',$Preanaliticasmico->id)->get();
+        return view('centrosreferencia.preanaliticagen.show', compact('Preanaliticasmico','analitica'));
+    }
+
+    public function edit(Preanaliticagen $Preanaliticasmico){
+        dd($Preanaliticasmico);
+        return view('centrosreferencia.preanaliticagen.edit', compact('Preanaliticasmico'));
+    }
+}
--- routes/Modules/Plataformas.php
+++ routes/Modules/Plataformas.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\CentrosReferencia\Analitica\AnaliticaController;
 use App\Http\Controllers\CentrosReferencia\Analiticap\AnaliticapController;
 use App\Http\Controllers\CentrosReferencia\Analiticatoxico\AnaliticatoxicoController;
 use App\Http\Controllers\CentrosReferencia\Analiticamicolo\AnaliticamicoloController;
+use App\Http\Controllers\CentrosReferencia\Preanaliticagen\PreanaliticagenController;
 use App\Http\Controllers\CentrosReferencia\Analiticatoxicop\AnaliticatoxicopController;
 use App\Http\Controllers\CentrosReferencia\Postanalitica\PostanaliticaController;
 use App\Http\Controllers\CentrosReferencia\Postanaliticap\PostanaliticapController;
@@ -80,6 +81,9 @@ Route::middleware(['auth'])->group(function () {
 
         //CRNs Plataformas Preanalitica Micologia
         Route::resource('preanaliticasmico', PreanaliticamicoController::class)->parameters(['preanaliticas' => 'preanalitica'])->names('preanaliticamico');
+
+        //CRNs Plataformas Preanalitica Gebsbio
+        Route::resource('preanaliticagen', PreanaliticagenController::class)->parameters(['preanaliticas' => 'preanalitica'])->names('preanaliticagen');
 
         //CRNs Plataformas Preanalitica CD4
         Route::resource('preanaliticascd4', Preanaliticacd4Controller::class)->parameters(['preanaliticas' => 'preanalitica'])->names('preanaliticacd4');
